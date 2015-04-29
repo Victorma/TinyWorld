9923564ee4aebcc58a5e5d04107105e3a53f8fe5
juzaru18 <juzaru18@gmail.com>
2015-04-29 23:56:27 +0200

    Reglas, tareas y objetivos para Agente GameManager

--- Server/Icaro/src/icaro/aplicaciones/informacion/game_manager/VocabularioControlGameManager.java
+++ Server/Icaro/src/icaro/aplicaciones/informacion/game_manager/VocabularioControlGameManager.java
@@ -2,9 +2,10 @@ package icaro.aplicaciones.informacion.game_manager;
 
 public class VocabularioControlGameManager {
 	
-	public static final String NombreTipoNotificacionSolicitarJuego="Solicitar Juego";
+	public static final String NombreTipoNotificacionIniciarJuego="Iniciar Juego";
 	public static final String NombreTipoNotificacionIniciarMinions="Iniciar Minions";
         public static final String NombreTipoNotificacionTerminarJuego="Terminar Juego";
+        public static final String IdentRecursoPersistenciaBD="Obtengo datos del usuario";
 	public static final String TextoInicioJuego="He iniciado el juego: ";
 	public static final String IdentAgenteGamenManager = "AgenteAplicacionGamenManager";
 	
