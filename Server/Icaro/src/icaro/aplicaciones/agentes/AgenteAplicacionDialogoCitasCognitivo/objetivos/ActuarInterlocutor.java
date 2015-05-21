740016a78615204c91deae4b9e8fafddf005b194
gorkinovich <gorkasg@yahoo.es>
2015-05-22 00:56:31 +0200

    Cleaning an unused agent

--- Server/Icaro/src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoCitasCognitivo/objetivos/ActuarInterlocutor.java
+++
@@ -1,9 +0,0 @@
-package icaro.aplicaciones.agentes.AgenteAplicacionDialogoCitasCognitivo.objetivos;
-
-import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
-
-public class ActuarInterlocutor extends Objetivo {
-    public ActuarInterlocutor() {
-        super.setgoalId("ActuarInterlocutor");
-    }
-}
--- Server/Icaro/src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoCitasCognitivo/objetivos/ObtenerInfoInterlocutor.java
+++
@@ -1,9 +0,0 @@
-package icaro.aplicaciones.agentes.AgenteAplicacionDialogoCitasCognitivo.objetivos;
-
-import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
-
-public class ObtenerInfoInterlocutor extends Objetivo {
-    public ObtenerInfoInterlocutor() {
-        super.setgoalId("AutorizarAccesoUsuarios");
-    }
-}
