880a4708225b9c034e3d2aae312857512b408512
gorkinovich <gorkasg@yahoo.es>
2015-05-22 16:34:32 +0200

    Fixing Partida class?

--- Server/Icaro/src/icaro/aplicaciones/informacion/game_manager/Partida.java
+++ Server/Icaro/src/icaro/aplicaciones/informacion/game_manager/Partida.java
@@ -9,30 +9,27 @@ import icaro.infraestructura.entidadesBasicas.descEntidadesOrganizacion.jaxb.Des
 import icaro.infraestructura.patronAgenteCognitivo.factoriaEInterfacesPatCogn.AgenteCognitivo;
 import icaro.infraestructura.patronAgenteCognitivo.factoriaEInterfacesPatCogn.FactoriaAgenteCognitivo;
 import icaro.infraestructura.patronAgenteCognitivo.factoriaEInterfacesPatCogn.ItfUsoAgenteCognitivo;
-import icaro.infraestructura.patronAgenteReactivo.factoriaEInterfaces.ItfGestionAgenteReactivo;
 import icaro.infraestructura.recursosOrganizacion.configuracion.imp.ClaseGeneradoraConfiguracion;
 import icaro.infraestructura.recursosOrganizacion.repositorioInterfaces.ItfUsoRepositorioInterfaces;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class Partida {
+    //****************************************************************************************************
+    // Types:
+    //****************************************************************************************************
 
     public enum EstadoPartida {
-
         SIN_COMPLETAR, COMPLETADA
     }
 
-    //Clase privada
-    private class ObjPartida {
+    public class ObjPartida {
 
         public GameEvent evento;
         public boolean completado;
 
         ObjPartida(GameEvent evento) {
-            this.evento = evento;
-            this.completado = false;
+            this(evento, false);
         }
 
         ObjPartida(GameEvent evento, boolean estado) {
@@ -41,24 +38,30 @@ public class Partida {
         }
     }
 
-    //Parametros
+    //****************************************************************************************************
+    // Fields:
+    //****************************************************************************************************
+
     public List<ObjPartida> objetivos;
     public List<String> minions;
     public EstadoPartida estado = EstadoPartida.SIN_COMPLETAR;
 
-    //Metodos
+    //****************************************************************************************************
+    // Constructors:
+    //****************************************************************************************************
+
     public Partida(AgenteCognitivo agente, ItfUsoRepositorioInterfaces repoInterfaces, GameEvent event) {
         GameEvent[] objtmp = (GameEvent[]) event.getParameter("objetivos");
         if (objtmp == null) {
             objtmp = new GameEvent[0];
         }
 
-        List<MinionInfo> mintmp = new ArrayList<MinionInfo>();
+        List<MinionInfo> mintmp = new ArrayList<>();
         for (Object o : (List<Object>) event.getParameter("minions")) {
             mintmp.add((MinionInfo) o);
         }
 
-        objetivos = new ArrayList<ObjPartida>();
+        objetivos = new ArrayList<>();
 
         for (GameEvent ge : objtmp) {
             objetivos.add(new ObjPartida(ge));
@@ -68,7 +71,7 @@ public class Partida {
 
         try {
             DescComportamientoAgente dca = ClaseGeneradoraConfiguracion.instance().getDescComportamientoAgente("AgenteAplicacionMinion");
-            minions = new ArrayList<String>();
+            minions = new ArrayList<>();
             MinionContext mc = new MinionContext(agente, agente.getIdentAgente());
             for (MinionInfo mi : mintmp) {
                 DescInstanciaAgente descInstanciaAgente = new DescInstanciaAgente();
@@ -82,14 +85,18 @@ public class Partida {
                 ItfUsoAgenteCognitivo itfMinion = (ItfUsoAgenteCognitivo) repoInterfaces.obtenerInterfazUso(minionName);
                 AgenteCognitivo gestionMinion = (AgenteCognitivo) repoInterfaces.obtenerInterfazGestion(minionName);
                 gestionMinion.arranca();
-                
+
                 itfMinion.aceptaMensaje(new MensajeSimple(mi, agente.getIdentAgente(), minionName));
                 itfMinion.aceptaMensaje(new MensajeSimple(mc, agente.getIdentAgente(), minionName));
             }
-        } catch (Exception ex) {
-        	// 
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
         }
     }
+
+    //****************************************************************************************************
+    // Methods:
+    //****************************************************************************************************
 
     public void addObjetivo(GameEvent objetivo) {
         objetivos.add(new ObjPartida(objetivo));
@@ -100,45 +107,32 @@ public class Partida {
     }
 
     public void validarObjetivo(GameEvent objetivo) {
-        for (ObjPartida o : objetivos) {
-            if (o.evento.isNameEquals(objetivo.getName())) {
-
-                Collection<String> parameters = o.evento.getParameters();
-
-                boolean c = true;
-                for (String p : parameters) {
-                    if (objetivo.getParameter(p) != null) {
-                        if (!((String) objetivo.getParameter(p)).equalsIgnoreCase((String) o.evento.getParameter(p))) {
-                            c = false;
-                            break;
-                        }
-                    } else {
-                        c = false;
+        for (ObjPartida item : objetivos) {
+            if (item.evento.isNameEquals(objetivo.getName())) {
+                boolean completed = true;
+                for (String key : item.evento.getParameters()) {
+                    String objParam = (String) objetivo.getParameter(key);
+                    String eventParam = (String) item.evento.getParameter(key);
+                    if (objParam == null || !objParam.equalsIgnoreCase(eventParam)) {
+                        completed = false;
                         break;
                     }
                 }
-
-                o.completado = c;
-                break;
+                item.completado = completed;
             }
         }
-
         this.estado = objetivosCompletados() ? EstadoPartida.COMPLETADA : EstadoPartida.SIN_COMPLETAR;
     }
 
     public boolean objetivosCompletados() {
-    	// TODO change to true when objectives are planned
-        boolean completados = false;
         for (ObjPartida o : objetivos) {
             if (o.completado == false) {
-                completados = false;
-                break;
+                return false;
             }
         }
-        return completados;
+        return true;
     }
 
     public void terminaPartida() {
-
     }
 }
