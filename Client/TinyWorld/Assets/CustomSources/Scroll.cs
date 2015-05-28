74d3b1e5c7a79f870a2acfadc71d463d987fd575
ricom10 <ricoriv17@gmail.com>
2015-05-28 23:46:48 +0200

    Animation
    Animation of Menu with two textures

---
+++ Client/TinyWorld/Assets/CustomSources/Scroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public float velocidad = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * velocidad, 0);
+	}
+}
